41319c7279a271e54cb9316fe264f5d7d6e81cf1
Taulant74 <taulantr2004@gmail.com>
2026-02-16 00:00:44 +0100

    Cloudinary configuration from .env file

--- backend/PersonalPodcast/Program.cs
+++ backend/PersonalPodcast/Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using PersonalPodcast.Data;
+using DotNetEnv;
+
+// e loadim .env file ku e kena rujt konfigurimin e cloudinary
+Env.Load();
+
+Console.WriteLine("Cloudinary name = " + Environment.GetEnvironmentVariable("CLOUDINARY_CLOUD_NAME"));
+
 
 var builder = WebApplication.CreateBuilder(args);
 
