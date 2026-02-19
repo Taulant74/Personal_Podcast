b3836b4a6bbf37223543a58f333082844c1aa651
DevAndi-m <andimorina093@gmail.com>
2026-02-19 03:12:56 +0100

    - app.js fixed

--- frontend/personalpodcast/src/App.js
+++ frontend/personalpodcast/src/App.js
@@ -1,13 +1,8 @@
 
 
-import logo from './logo.svg';
-
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import EpisodesUpload from './pages/EpisodesUpload';
-
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import EpisodesUpload from "./pages/EpisodesUpload";
 import HomePage from "./pages/HomePage";
 
 function App() {
@@ -106,33 +101,6 @@ function App() {
         </Routes>
       </div>
     </Router>
-
-function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Podcast Platform</h2>
-      <p>Welcome to the homepage.</p>
-      <Link to="/episodes">Go to Upload & Search Episodes</Link>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav style={{ padding: 10, background: "#f5f5f5" }}>
-          <Link to="/" style={{ marginRight: 15 }}>Home</Link>
-          <Link to="/episodes">Episodes</Link>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/episodes" element={<EpisodesUpload />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+);}
 
 export default App;
